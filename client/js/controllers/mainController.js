06f418291609ab23b5f92021bbbb6c790aa0dacd
Kristin Sztengel <kcsztengel@gmail.com>
2016-12-28 11:58:15 -0700

    artist name getting to mainController

--- client/js/controllers/mainController.js
+++ client/js/controllers/mainController.js
@@ -1,10 +1,12 @@
 'use strict'
 app.controller('MainController', function($scope, MusicService) {
     console.log("in the MainController");
+
     $scope.artist = {}
+
     $scope.submitArtist = function(artist) {
         console.log('artist', artist);
-        MusicService.artistName().then(artist => {
+        MusicService.artistName(artist).then(artist => {
 
         })
     }
--- client/js/services/musicService.js
+++ client/js/services/musicService.js
@@ -3,7 +3,7 @@ app.service('MusicService', function($http) {
   console.log("in the MusicService")
     return {
         artistName: function(artist) {
-            return $http.get(`http://localhost:3000/musicFetcher/?artist=${artist}`);
+            return $http.get(`http://localhost:3000/musicFetcher/?artist=${artist.name}`);
         }
 
 
