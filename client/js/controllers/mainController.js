6d9ea2925cd7ab4fbc2639c066f071bc00d7ea33
Kristin Sztengel <kcsztengel@gmail.com>
2017-01-05 15:09:32 -0700

    page loader working

--- client/js/controllers/mainController.js
+++ client/js/controllers/mainController.js
@@ -2,11 +2,13 @@
 app.controller('MainController', function($scope, MusicService, $http, $rootScope, ScraperService, SentimentService, SpotifyService, $sanitize, $timeout) {
 
     $scope.artist = {}
-
+    $scope.artist.loading = false
     $scope.submitArtist = function(artist) {
         console.log('artist', artist);
+        $scope.artist.loading = true
 
         MusicService.artistName($scope.artist).then(artist => {
+
 
             MusicService.musixmatch($scope.artist).then(data => {
 
@@ -16,12 +18,12 @@ app.controller('MainController', function($scope, MusicService, $http, $rootScop
 
                 console.log('trackInfo', $rootScope.trackInfo);
 
-
             })
 
         })
     }
 
+    $scope.artist.loading = false
 
     $scope.scraper = function(track) {
         ScraperService.getImage(track).then(image => {
@@ -71,57 +73,58 @@ app.controller('MainController', function($scope, MusicService, $http, $rootScop
 
         })
     }
-  //   $scope.myJson = {
-  //         globals: {
-  //             shadow: false,
-  //             fontFamily: "Verdana",
-  //             fontWeight: "100"
-  //         },
-  //         type: "pie",
-  //         backgroundColor: "#fff",
-  //
-  //         legend: {
-  //             layout: "x5",
-  //             position: "50%",
-  //             borderColor: "transparent",
-  //             marker: {
-  //                 borderRadius: 10,
-  //                 borderColor: "transparent"
-  //             }
-  //         },
-  //         tooltip: {
-  //             text: "%v requests"
-  //         },
-  //         plot: {
-  //             refAngle: "-90",
-  //             borderWidth: "0px",
-  //             valueBox: {
-  //                 placement: "in",
-  //                 text: "%npv %",
-  //                 fontSize: "15px",
-  //                 textAlpha: 1,
-  //             }
-  //         },
-  //         series: [{
-  //             text: "10.0.0.80",
-  //             values: [4660],
-  //             backgroundColor: "#FA6E6E #FA9494",
-  //         }, {
-  //             text: "167.114.156.198",
-  //             values: [1807],
-  //             backgroundColor: "#F1C795 #feebd2"
-  //         }, {
-  //             text: "103.24.77.25",
-  //             values: [1611],
-  //             backgroundColor: "#FDAA97 #FC9B87"
-  //         }, {
-  //             text: "46.4.68.142",
-  //             values: [1341],
-  //             backgroundColor: "#28C2D1"
-  //         }, {
-  //             text: "10.0.0.117",
-  //             values: [1269],
-  //             backgroundColor: "#D2D6DE",
-  //         }]
-  //     };
+    $scope.myJson = {
+          globals: {
+              shadow: false,
+              fontFamily: "Verdana",
+              fontWeight: "100"
+          },
+          type: "pie",
+          backgroundColor: "#fff",
+
+
+          legend: {
+              layout: "x1",
+              position: "1%",
+              borderColor: "transparent",
+              marker: {
+                  borderRadius: 5,
+                  borderColor: "transparent"
+              }
+          },
+          tooltip: {
+              text: "%v requests"
+          },
+          plot: {
+              refAngle: "-90",
+              borderWidth: "0px",
+              valueBox: {
+                  placement: "in",
+                  text: "%npv %",
+                  fontSize: "10px",
+                  textAlpha: 1,
+              }
+          },
+          series: [{
+              text: "anger",
+              values: [34],
+              backgroundColor: "#e53935",
+          }, {
+              text: "disgust",
+              values: [25],
+              backgroundColor: "#4caf50"
+          }, {
+              text: "fear",
+              values: [16],
+              backgroundColor: "#ffeb3b"
+          }, {
+              text: "joy",
+              values: [2],
+              backgroundColor: "#7e57c2"
+          }, {
+              text: "sadness",
+              values: [31],
+              backgroundColor: "#1976d2",
+          }]
+      };
   });
