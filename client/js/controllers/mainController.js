7df9e4895741e07d7fa8906c816add7b0e2b5a68
Kristin Sztengel <kcsztengel@gmail.com>
2017-01-05 15:55:28 -0700

    commit early and often

--- client/js/controllers/mainController.js
+++ client/js/controllers/mainController.js
@@ -28,11 +28,10 @@ app.controller('MainController', function($scope, MusicService, $http, $rootScop
     $scope.scraper = function(track) {
         ScraperService.getImage(track).then(image => {
             $rootScope.image = image.data
-                // console.log("image", image.data);
-          $rootScope.lyrics = image.data.lyrics
+            $rootScope.lyrics = image.data.lyrics
             var image = image.data.img
-                console.log('lyrics:', $rootScope.lyrics);
-                console.log('image', image);
+            console.log('lyrics:', $rootScope.lyrics);
+            console.log('image', image);
 
 
         }).then(() => {
@@ -42,7 +41,6 @@ app.controller('MainController', function($scope, MusicService, $http, $rootScop
             SentimentService.getSentiment(lyrics).then(emotions => {
                 console.log('emotions:', emotions);
                 $rootScope.emotions = emotions.data.docEmotions
-
                 console.log('rootScope', emotions.data.docEmotions);
 
                 var anger = emotions.data.docEmotions.anger
@@ -66,65 +64,65 @@ app.controller('MainController', function($scope, MusicService, $http, $rootScop
             $rootScope.title = title.tracks
             console.log(title.tracks);
 
-          var snip = $sanitize(title.tracks.items["0"].preview_url)
-          $rootScope.snippet = angular.copy(snip.trim())
+            var snip = $sanitize(title.tracks.items["0"].preview_url)
+            $rootScope.snippet = angular.copy(snip.trim())
             console.log('snippet', $rootScope.snippet);
             console.log('title', title);
 
         })
     }
     $scope.myJson = {
-          globals: {
-              shadow: false,
-              fontFamily: "Verdana",
-              fontWeight: "100"
-          },
-          type: "pie",
-          backgroundColor: "#fff",
+        globals: {
+            shadow: false,
+            fontFamily: "Verdana",
+            fontWeight: "100"
+        },
+        type: "pie",
+        backgroundColor: "#fff",
 
 
-          legend: {
-              layout: "x1",
-              position: "1%",
-              borderColor: "transparent",
-              marker: {
-                  borderRadius: 5,
-                  borderColor: "transparent"
-              }
-          },
-          tooltip: {
-              text: "%v requests"
-          },
-          plot: {
-              refAngle: "-90",
-              borderWidth: "0px",
-              valueBox: {
-                  placement: "in",
-                  text: "%npv %",
-                  fontSize: "10px",
-                  textAlpha: 1,
-              }
-          },
-          series: [{
-              text: "anger",
-              values: [34],
-              backgroundColor: "#e53935",
-          }, {
-              text: "disgust",
-              values: [25],
-              backgroundColor: "#4caf50"
-          }, {
-              text: "fear",
-              values: [16],
-              backgroundColor: "#ffeb3b"
-          }, {
-              text: "joy",
-              values: [2],
-              backgroundColor: "#7e57c2"
-          }, {
-              text: "sadness",
-              values: [31],
-              backgroundColor: "#1976d2",
-          }]
-      };
-  });
+        legend: {
+            layout: "x1",
+            position: "1%",
+            borderColor: "transparent",
+            marker: {
+                borderRadius: 5,
+                borderColor: "transparent"
+            }
+        },
+        tooltip: {
+            text: "%v requests"
+        },
+        plot: {
+            refAngle: "-90",
+            borderWidth: "0px",
+            valueBox: {
+                placement: "in",
+                text: "%npv %",
+                fontSize: "10px",
+                textAlpha: 1,
+            }
+        },
+        series: [{
+            text: "anger",
+            values: [34],
+            backgroundColor: "#e53935",
+        }, {
+            text: "disgust",
+            values: [25],
+            backgroundColor: "#4caf50"
+        }, {
+            text: "fear",
+            values: [16],
+            backgroundColor: "#ffeb3b"
+        }, {
+            text: "joy",
+            values: [2],
+            backgroundColor: "#7e57c2"
+        }, {
+            text: "sadness",
+            values: [31],
+            backgroundColor: "#1976d2",
+        }]
+    };
+});
