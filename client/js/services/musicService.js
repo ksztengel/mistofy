b21b41f7d4059fadfe60093daf241968673d0ded
Kristin Sztengel <kcsztengel@gmail.com>
2017-01-09 10:51:41 -0700

    https for local

--- client/js/services/musicService.js
+++ client/js/services/musicService.js
@@ -3,11 +3,11 @@ app.service('MusicService', function($http) {
   console.log("in the MusicService")
     return {
         artistName: function(artist) {
-            return $http.get(`http://localhost:3000/musicFetcher/?artist=${artist.name}`);
+            return $http.get(`https://localhost:3000/musicFetcher/?artist=${artist.name}`);
         },
 
         musixmatch: function(artist){
-           return $http.get(`http://localhost:3000/musicFetcher/?artist=${artist.name}`)
+           return $http.get(`https://localhost:3000/musicFetcher/?artist=${artist.name}`)
 
         }
     }
--- client/js/services/scraperService.js
+++ client/js/services/scraperService.js
@@ -3,7 +3,7 @@ app.service('ScraperService', function($http) {
   console.log("in the ScraperService")
     return {
         getImage: function(track) {
-            return $http.post('http://localhost:3000/scraper', {track});
+            return $http.post('https://localhost:3000/scraper', {track});
 
 
         }
