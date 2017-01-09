b58fca0ede5f34369aa3c27b9144bb4215dc047b
Kristin Sztengel <kcsztengel@gmail.com>
2017-01-09 11:07:04 -0700

    changed localhost paths to deployed

--- client/js/services/musicService.js
+++ client/js/services/musicService.js
@@ -3,11 +3,13 @@ app.service('MusicService', function($http) {
   console.log("in the MusicService")
     return {
         artistName: function(artist) {
-            return $http.get(`https://localhost:3000/musicFetcher/?artist=${artist.name}`);
+            // return $http.get(`http://localhost:3000/musicFetcher/?artist=${artist.name}`);
+            return $http.get(`https://ksztengel-mistofy.herokuapp.com/musicFetcher/?artist=${artist.name}`);
         },
 
         musixmatch: function(artist){
-           return $http.get(`https://localhost:3000/musicFetcher/?artist=${artist.name}`)
+          // return $http.get(`http://localhost:3000/musicFetcher/?artist=${artist.name}`);
+           return $http.get(`https://ksztengel-mistofy.herokuapp.com/musicFetcher/?artist=${artist.name}`)
 
         }
     }
--- client/js/services/sentimentService.js
+++ client/js/services/sentimentService.js
@@ -3,7 +3,8 @@ app.service('SentimentService', function($http) {
   console.log("in the SentimentService")
     return {
         getSentiment: function(lyrics) {
-            return $http.post('https://localhost:3000/sentimentFetcher', {lyrics});
+            // return $http.post('http://localhost:3000/sentimentFetcher', {lyrics});
+            return $http.post('https://ksztengel-mistofy.herokuapp.com/sentimentFetcher', {lyrics});
 
 
         }
